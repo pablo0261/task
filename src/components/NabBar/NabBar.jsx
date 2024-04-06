5fdb701094493f0ea36f4b2aced49d1f3f7bd07b
pablo0261 <pablobesler@gmail.com>
2024-04-06 01:49:19 -0300

    modificando el edity create task. falta creat un componente edittask para cada itemTask

--- src/components/NabBar/NabBar.jsx
+++ src/components/NabBar/NabBar.jsx
@@ -1,5 +1,15 @@
 import style from "./NabBar.module.sass";
+import { useState } from "react";
+import CreateTask from "../../components/Tasks/CreateTasks/CreatTasks"
+
+
 const NabBar = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const toggleFormVisibility = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div className={style.backWrapper}>
       <div className={style.divInputs}>
@@ -8,8 +18,9 @@ const NabBar = () => {
         <input className={style.Inputs2} placeholder="Filtrar por email..." />
       </div>
       <div className={style.divButtonCreateTask}>
-        <button className={style.ButtonCreateTask}>
+        <button onClick={toggleFormVisibility} className={style.ButtonCreateTask}>
          + Tarea
+         {showForm && <CreateTask setShowForm={setShowForm} />}
         </button>
       </div>
     </div>
