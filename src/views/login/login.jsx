e99cbe32ad9d0b0c877eeb0cb10261ad0cbeb56b
pablo0261 <pablobesler@gmail.com>
2024-04-17 23:01:24 -0300

    correccion estilo login y signin

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -1,5 +1,5 @@
 import { Formik, Form } from "formik";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FormikInput from "../../components/formik/formikInput.jsx";
 import loginValidationSchema from "./loginValidation.js";
 import helpers from "../../helpers/routesFront.js";
@@ -8,9 +8,8 @@ import axios from "axios";
 import { useGoogleLogin } from "@react-oauth/google";
 import Swal from "sweetalert2";
 import { useEffect, useState } from "react";
-import StoreItem from '../../helpers/LocalStorage.js'
+import StoreItem from "../../helpers/LocalStorage.js";
 import { jwtDecode } from "jwt-decode";
-
 
 const initialValues = {
   email: "",
@@ -19,77 +18,88 @@ const initialValues = {
 };
 
 function LogIn() {
-  const navigate =  useNavigate();
+  const navigate = useNavigate();
   const [rememberMe, setRememberMe] = useState(false);
-  
+
   useEffect(() => {
-    const tokenStorage = localStorage.getItem('token');
+    const tokenStorage = localStorage.getItem("token");
     if (tokenStorage) {
-      console.log("tokenStorage", tokenStorage)
+      console.log("tokenStorage", tokenStorage);
       try {
         const decodedToken = jwtDecode(tokenStorage);
         const username = decodedToken.username;
-        console.log('Usuario ya autenticado:', decodedToken.username);
-        console.log('typeAdmin:', decodedToken.typeAdmin);
+        console.log("Usuario ya autenticado:", decodedToken.username);
+        console.log("typeAdmin:", decodedToken.typeAdmin);
         Swal.fire({
           icon: "success",
           title: `¡Bienvenido de nuevo ${username}!`,
           text: `Vamos a MyTask?`,
           showDenyButton: true,
-          confirmButtonText: 'SI!',
+          confirmButtonText: "SI!",
           denyButtonText: `Volver al Login`,
         }).then((result) => {
           if (result.isConfirmed) {
             navigate("/myTasks");
           } else if (result.isDenied) {
-            navigate("/login"); 
+            navigate("/login");
           }
         });
       } catch (error) {
-        console.log('Error al decodificar el token:', error);
+        console.log("Error al decodificar el token:", error);
       }
     }
   }, [navigate]);
 
-
-
-
   const handleLogin = async (values) => {
     try {
-       
-      console.log(values, "values")
+      console.log(values, "values");
       const response = await axios.post("http://localhost:3000/login", values);
-      console.log(response.status, "response.status")
+      console.log(response.status, "response.status");
       if (response.status === 200) {
         const token = response.data.token;
-        console.log(rememberMe, "remenberMe")
+        console.log(rememberMe, "remenberMe");
         console.log("Token recibido del servidor:", token);
-        
-        localStorage.setItem('token', token);
-        
+
+        localStorage.setItem("token", token);
+
         if (rememberMe) {
           const passwordUser = response.data.password;
           const email = response.data.email;
-          
-          console.log("Email y password recibida del servidor:", email, passwordUser);
-          
-          localStorage.setItem(StoreItem.passwordUser, JSON.stringify(passwordUser));  
-          localStorage.setItem(StoreItem.email, JSON.stringify(email));  
+
+          console.log(
+            "Email y password recibida del servidor:",
+            email,
+            passwordUser
+          );
+
+          localStorage.setItem(
+            StoreItem.passwordUser,
+            JSON.stringify(passwordUser)
+          );
+          localStorage.setItem(StoreItem.email, JSON.stringify(email));
         }
-        
+
         navigate("/myTasks");
       } else {
-        console.error("Error de inicio de sesión en el servidor:", response.statusText);
+        console.error(
+          "Error de inicio de sesión en el servidor:",
+          response.statusText
+        );
         // Mostrar el mensaje de error proporcionado por el servidor
         Swal.fire({
           icon: "error",
           title: "Error",
-          text: response.data.error || "Ocurrió un error al iniciar sesión. Por favor, inténtalo de nuevo.",
+          text:
+            response.data.error ||
+            "Ocurrió un error al iniciar sesión. Por favor, inténtalo de nuevo.",
         });
       }
     } catch (error) {
-      console.error("Error al enviar los datos de inicio de sesión al servidor:", error);
-      
+      console.error(
+        "Error al enviar los datos de inicio de sesión al servidor:",
+        error
+      );
+
       // Mostrar el mensaje de error
       Swal.fire({
         icon: "error",
@@ -109,25 +119,33 @@ function LogIn() {
         redirect_uri: "your_redirect_uri",
         grant_type: "authorization_code",
       });
-      
+
       if (response.status === 200) {
         // El servidor ha validado el código y ha respondido con un token JWT
         console.log("Token recibido del servidor:", response.data.token);
         // Aquí puedes manejar la respuesta del servidor, como guardar el token en el almacenamiento local o redirigir a otra página
       } else {
         // Maneja errores de inicio de sesión con Google
-        console.error("Error de inicio de sesión con Google en el servidor:", response.statusText);
-        
+        console.error(
+          "Error de inicio de sesión con Google en el servidor:",
+          response.statusText
+        );
+
         // Mostrar el mensaje de error proporcionado por el servidor
         Swal.fire({
           icon: "error",
           title: "Error",
-          text: response.data.error || "Ocurrió un error al iniciar sesión con Google. Por favor, inténtalo de nuevo más tarde.",
+          text:
+            response.data.error ||
+            "Ocurrió un error al iniciar sesión con Google. Por favor, inténtalo de nuevo más tarde.",
         });
       }
     } catch (error) {
-      console.error("Error al enviar el código de autorización al servidor:", error);
-      
+      console.error(
+        "Error al enviar el código de autorización al servidor:",
+        error
+      );
+
       // Mostrar el mensaje de error
       Swal.fire({
         icon: "error",
@@ -136,7 +154,7 @@ function LogIn() {
       });
     }
   };
-  
+
   const onGoogleLogin = useGoogleLogin({
     onSuccess: onGoogleLoginSuccess,
     flow: "auth-code",
@@ -144,7 +162,7 @@ function LogIn() {
     cookiepolicy: "none",
     accesstype: "offline",
   });
-  
+
   return (
     <div className={style.background}>
       <div className={style.marginContainer}>
@@ -153,12 +171,9 @@ function LogIn() {
         </div>
         <div className={style.quoteWrapper}>
           <h2 className={style.quote}>
-            “Tu agenda, tu <span className={style.strong}>control</span>{" "}
-            total”
+            “Tu agenda, tu <span className={style.strong}>control</span> total”
           </h2>
-          <h3 className={style.slogan}>
-            Simplificando la gestión de Tareas.
-          </h3>
+          <h3 className={style.slogan}>Simplificando la gestión de Tareas.</h3>
         </div>
         <div className={style.logInWrapper}>
           <div className={style.form}>
@@ -184,17 +199,26 @@ function LogIn() {
                     <div className={style.rememberMe}>
                       <label>
                         <input
+                          className={style.inputRememberMe}
                           type="checkbox"
                           name="rememberMe"
                           checked={rememberMe}
                           onChange={(e) => setRememberMe(e.target.checked)}
                         />
-                        <span className={style.rememberMeText}>Recordar mis datos</span>
                       </label>
+                      <p className={style.rememberMeText}>Recordarme</p>
                     </div>
                     <button type="submit" className={style.submitBtn}>
                       Ingresar
                     </button>
+                    <div className={style.socialApps}>
+                      <button className={style.google} onClick={onGoogleLogin}>
+                        Ingresar con Google
+                      </button>
+                      <button className={style.facebook} onClick={onGoogleLogin}>
+                        Ingresar con Google
+                      </button>
+                    </div>
                   </Form>
                 );
               }}
@@ -204,19 +228,14 @@ function LogIn() {
               <div className={style.letter}>o</div>
               <div className={style.line}></div>
             </div>
-            <div className={style.socialApps}>
-              <button className={style.google} onClick={onGoogleLogin}>
-                Ingresar con Google
-              </button>
+            <div className={style.questionWrapper}>
+              <p className={style.question}>
+                ¿No tienes una cuenta?{" "}
+                <Link to={helpers.signIn} className={style.link}>
+                  Registrate
+                </Link>{" "}
+              </p>
             </div>
-          </div>
-          <div className={style.questionWrapper}>
-            <p className={style.question}>
-              ¿No tienes una cuenta?{" "}
-              <Link to={helpers.signIn} className={style.link}>
-                Registrate
-              </Link>{" "}
-            </p>
           </div>
         </div>
       </div>
